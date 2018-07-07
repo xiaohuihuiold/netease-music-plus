6dcc4370c03f8ef240f679099223171acd383839
xiaohuihuiold <xiaohuihuiold@gmail.com>
2018-07-07 11:02:49 +0800

    免重启生效模块更改

--- app/src/main/java/com/xhhold/xp/plus/Module.java
+++ app/src/main/java/com/xhhold/xp/plus/Module.java
@@ -2,8 +2,11 @@ package com.xhhold.xp.plus;
 
 import de.robv.android.xposed.XposedBridge;
 
+/**
+ * @author nameh
+ */
 public class Module {
     public Module(){
-
+        XposedBridge.log("测试加载");
     }
 }
--- app/src/main/java/com/xhhold/xp/plus/util/ModuleUtils.java
+++ app/src/main/java/com/xhhold/xp/plus/util/ModuleUtils.java
@@ -1,5 +1,8 @@
 package com.xhhold.xp.plus.util;
 
+/**
+ * @author nameh
+ */
 public class ModuleUtils {
     public static final String HOOK_PACKAGE = "com.netease.cloudmusic";
 }
